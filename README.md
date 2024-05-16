be639606126c6b17c2436e22895b0edbd89aee1f
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2024-05-15 21:20:25 -0400

    Add deb support

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
  Script that automatically downloads and installs Discord updates on Linux.
 
 # Features
-- Currently only works with tar.gz installs, support for deb installs is planned
-- If Discord is found in the current working directory, the script will install the update to that location, otherwise it will prompt the user to enter a path
+- Supports both tar.gz and deb installations
+- For a tar.gz installation, if Discord is found in the current working directory, the script will install the update to that location, otherwise it will prompt the user to enter a path
 - The script will terminate if the download fails for whatever reason
--- UpdateDiscord.sh
+++ UpdateDiscord.sh
@@ -1,23 +1,55 @@
 #!/bin/bash
-URL="https://discord.com/api/download/stable?platform=linux&format=tar.gz"
-DIR="$(pwd)"
+DEB=false
 
-while ! test -d "$DIR/Discord"; do
-	read -p "Discord installation not found. Enter the path you want to install to, excluding the Discord folder: " DIR
-done
+TarUpdate()
+{
+	URL="https://discord.com/api/download/stable?platform=linux&format=tar.gz"
+	DIR="$(pwd)"
 
-echo "Downloading Discord..."
-wget -q --show-progress $URL -O /tmp/discord.tar.gz
+	while ! test -d "$DIR/Discord"; do
+		read -p "Discord installation not found. Enter the path you want to install to, excluding the Discord folder: " DIR
+	done
 
-if test -f /tmp/discord.tar.gz; then
-	echo "Extracting update..."
-	tar -xf /tmp/discord.tar.gz
-	echo "Cleaning up..."
-	rm /tmp/discord.tar.gz
-	rm -rf /tmp/Discord
-	echo "Finished"
+	echo "Downloading Discord..."
+	wget -q --show-progress $URL -O /tmp/discord.tar.gz
+
+	if test -f /tmp/discord.tar.gz; then
+		echo "Extracting update..."
+		tar -xf /tmp/discord.tar.gz
+		echo "Cleaning up..."
+		rm /tmp/discord.tar.gz
+		rm -rf /tmp/Discord
+		echo "Finished"
+	else
+		echo "Failed to download update. Aborting."
+	fi
+}
+
+DebUpdate()
+{
+	URL="https://discord.com/api/download/stable?platform=linux&format=deb"
+	echo "Downloading Discord..."
+	wget -q --show-progress $URL -O /tmp/discord.deb
+	
+	if test -f /tmp/discord.deb; then
+		echo "Installing update..."
+		sudo dpkg -i /tmp/discord.deb
+		echo "Cleaning up..."
+		rm /tmp/discord.deb
+		echo "Finished"
+	else
+		echo "Failed to download update. Aborting."
+	fi
+}
+
+if command -v dpkg &> /dev/null; then
+	DEB=true
+fi
+
+if "$DEB"; then
+	DebUpdate
 else
-	echo "Failed to download update. Aborting."
+	TarUpdate
 fi
 
 read -p "Press any key to continue..."
