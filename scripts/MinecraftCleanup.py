fdc99f070e37b1089ed0c59a0ed7c45c0dc69071
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2026-08-01 11:20:30 -0400

    Revamp minecraft cleanup script
    Add Prism Launcher support and rewrite functions to ignore missing files instead of erroring.

--- scripts/MinecraftCleanup.py
+++ scripts/MinecraftCleanup.py
@@ -2,114 +2,108 @@ import json
 import os
 import platform
 import shutil
-import sys
 from pathlib import Path
 
-AppData = platform.system() == "Linux" and str( Path.home() ) or os.getenv( "APPDATA" )
-MinecraftPath = AppData + "/.minecraft"
-TechnicPath = AppData + "/.technic"
-MostRecentVersion = ""
-MostRecentSnapshot = ""
-ProfileTable = []
-
-def GetLatestVersion():
-	global MostRecentVersion, MostRecentSnapshot
-	path = MinecraftPath + "/versions/version_manifest_v2.json"
-	if not os.path.exists( path ):
-		print( f"Failed to find {path}. Make sure you run the game at least once before running the cleanup." )
-		sys.exit()
-	data = open( path, "r" )
-	newjson = json.loads( data.read() )
-	MostRecentVersion = newjson["latest"]["release"]
-	MostRecentSnapshot = newjson["latest"]["snapshot"]
-	data.close()
-
-def ParseJSON():
-	path = MinecraftPath + "/launcher_profiles.json"
-	data = open( path )
-	newjson = json.loads( data.read() )
-	profiles = newjson["profiles"]
-	for profile in profiles:
-		ProfileTable.append( newjson["profiles"][profile]["lastVersionId"] )
+def IsLinux():
+	return platform.system() == "Linux"
 
 def SafeRemoveFile( file ):
+	if not os.path.exists( file ):
+		return
 	try:
 		os.remove( file )
+		print( f"Deleting {file}" )
 	except PermissionError:
-		print( f"Couldn't delete {file}: Permission denied. Path might be a folder or the file might be in use." )
-	except FileNotFoundError:
-		print( f"Couldn't delete {file}: File not found." )
+		print( f"Couldn't delete {file}: Permission denied." )
 	except:
 		print( f"Something went wrong while deleting {file}. Skipping..." )
 
-def DeleteMinecraftLogs( technic ):
-	logpath = technic and TechnicPath + "/logs" or MinecraftPath + "/logs"
-	name = technic and "Technic" or "Minecraft"
-	foundlauncherlog = False
-	print( f"Checking for {name} log files..." )
-	if not os.path.exists( logpath ):
-		print( f"No {name} log files found. Skipping..." )
-		return
+AppData = IsLinux() and str( Path.home() ) or os.getenv( "APPDATA" )
+MinecraftPath = AppData + "/.minecraft"
+TechnicPath = AppData + "/.technic"
+PrismPath = AppData + "/.local/share/PrismLauncher" if IsLinux() else "/PrismLauncher"
 
-	if not technic:
-		launcherlogpath = MinecraftPath + "/launcher_log.txt"
-		if os.path.exists( launcherlogpath ):
-			print( "Deleting launcher_log.txt" )
-			SafeRemoveFile( launcherlogpath )
-			foundlauncherlog = True
-
-	getfiles = os.listdir( logpath )
-	if len( getfiles ) > 0:
-		for file in getfiles:
-			print( f"Deleting {name} log file: {file}" )
-			SafeRemoveFile( os.path.join( logpath, file ) )
-	elif not foundlauncherlog:
-		print( f"No {name} log files found. Skipping..." )
-
-	if technic:
-		modpackpath = TechnicPath + "/modpacks"
-		print( "Checking for Technic modpack log files..." )
-		if len( os.listdir( modpackpath ) ) == 0:
-			print( "No Technic modpacks found. Skipping..." )
-			return
-		for modpack in os.listdir( modpackpath ):
-			modpacklogpath = f"{modpackpath}/{modpack}/logs"
-			if not os.path.exists( modpacklogpath ):
-				print( f"Log folder not found for {modpack}. Skipping..." )
-				continue
-			logs = os.listdir( modpacklogpath )
-			if len( logs ) == 0:
-				print( f"No log files found for {modpack}. Skipping..." )
-				continue
-			for log in logs:
-				print( f"Deleting {name} modpack log file: {log}" )
-				SafeRemoveFile( os.path.join( modpacklogpath, log ) )
-
-def DeleteOldMinecraftVersions():
+def DeleteOldVersions():
+	# Get the latest release and snapshot versions so they aren't deleted
 	print( "Checking for old Minecraft versions..." )
-	foundversions = False
-	versionpath = MinecraftPath + "/versions"
-	getversions = os.listdir( versionpath )
-	for version in getversions:
-		finalpath = f"{versionpath}/{version}"
-		if version in ProfileTable or version == MostRecentVersion or version == MostRecentSnapshot or not os.path.isdir( finalpath ):
+	path = MinecraftPath + "/versions/version_manifest_v2.json"
+	if not os.path.exists( path ):
+		print( f"Failed to detect Minecraft launcher. Probably not installed?" )
+		return
+	data = open( path, "r" )
+	newJson = json.loads( data.read() )
+	MostRecentVersion = newJson["latest"]["release"]
+	MostRecentSnapshot = newJson["latest"]["snapshot"]
+	data.close()
+
+	# Parse launcher profiles
+	path = MinecraftPath + "/launcher_profiles.json"
+	data = open( path )
+	newJson = json.loads( data.read() )
+	profiles = newJson["profiles"]
+	ProfileTable = []
+	for profile in profiles:
+		ProfileTable.append( newJson["profiles"][profile]["lastVersionId"] )
+
+	# Delete unused versions
+	versionPath = MinecraftPath + "/versions"
+	getVersions = os.listdir( versionPath )
+	for version in getVersions:
+		finalPath = f"{versionPath}/{version}"
+		if version in ProfileTable or version == MostRecentVersion or version == MostRecentSnapshot or not os.path.isdir( finalPath ):
 			continue
 		print( f"Deleting old Minecraft version: {version}" )
-		shutil.rmtree( finalpath )
-		foundversions = True
-	if not foundversions:
-		print( "No old Minecraft versions found. Skipping..." )
+		shutil.rmtree( finalPath )
+
+def DeleteLogs():
+	# Minecraft launcher
+	print( "Checking for Minecraft launcher logs..." )
+	launcherLogPath = MinecraftPath + "/launcher_log.txt"
+	SafeRemoveFile( launcherLogPath )
+	logPath = MinecraftPath + "/logs"
+	if os.path.exists( logPath ):
+		getFiles = os.listdir( logPath )
+		for file in getFiles:
+			SafeRemoveFile( os.path.join( logPath, file ) )
+
+	# Technic
+	print( "Checking for Technic logs..." )
+	logPath = TechnicPath + "/logs"
+	getFiles = os.listdir( logPath )
+	if os.path.exists( logPath ):
+		for file in getFiles:
+			SafeRemoveFile( os.path.join( logPath, file ) )
+	modpackPath = TechnicPath + "/modpacks"
+	print( "Checking for Technic modpack logs..." )
+	if os.path.exists( modpackPath ):
+		for modpack in os.listdir( modpackPath ):
+			modpackLogPath = f"{modpackPath}/{modpack}/logs"
+			logs = os.listdir( modpackLogPath )
+			for l in logs:
+				SafeRemoveFile( os.path.join( modpackLogPath, l ) )
+
+	# Prism Launcher
+	print( "Checking for Prism Launcher logs..." )
+	logPath = PrismPath + "/logs"
+	getFiles = os.listdir( logPath )
+	if os.path.exists( logPath ):
+		for file in getFiles:
+			SafeRemoveFile( os.path.join( logPath, file ) )
+	instancePath = PrismPath + "/instances"
+	print( "Checking for Prism Launcher instance logs..." )
+	if os.path.exists( instancePath ):
+		for instance in os.listdir( instancePath ):
+			if instance == ".tmp" or not os.path.isdir( f"{instancePath}/{instance}" ):
+				continue
+			instanceLogPath = f"{instancePath}/{instance}/minecraft/logs"
+			logs = os.listdir( instanceLogPath )
+			if os.path.exists( instanceLogPath ):
+				for l in logs:
+					SafeRemoveFile( os.path.join( instanceLogPath, l ) )
 
 if __name__ == "__main__":
-	promptBypass = "-y" in sys.argv
-	print( "Minecraft Cleanup Utility | Copyright (c) 2020-2026 OPGman" )
-	GetLatestVersion()
-	ParseJSON()
-	if promptBypass or input( "Check for Minecraft log files? (y/n)" ) == "y":
-		DeleteMinecraftLogs( False )
-	if promptBypass or input( "Check for Technic log files? (y/n)" ) == "y":
-		DeleteMinecraftLogs( True )
-	if promptBypass or input( "Check for old Minecraft versions? (y/n)" ) == "y":
-		DeleteOldMinecraftVersions()
+	print( "Minecraft Cleanup Utility | Copyright (c) 2020-2026 OPGman | Licensed under the MIT License" )
+	DeleteOldVersions()
+	DeleteLogs()
 	print( "Process complete. Press any key to continue..." )
 	input()
