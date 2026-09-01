1c898eb41500a53feec6f1fc64cbbc8b889ca70a
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2026-08-31 21:43:07 -0400

    Add more checks to detect the correct link

--- scripts/SongsterrClicker.js
+++ scripts/SongsterrClicker.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Songsterr Auto Clicker
-// @version      1.0
+// @version      1.0.1
 // @description  Auto clicks Songsterr popups.
 // @author       OPGman
 // @grant        none
@@ -12,7 +12,7 @@
     function clicker() {
         let list = document.querySelectorAll( "a" );
         for ( let a of list ) {
-            if ( a.href == window.location.href ) {
+            if ( a.href == window.location.href && a.classList.length == 0 && !a.hasAttribute( "id" ) ) {
                 a.click();
                 break;
             }
